bc32baf55cb9bca547d292deabdc8be5fb653288
AntoineBrosseau <antoine.brosseau@edu.ece.fr>
2026-01-14 15:19:19 +0100

    etape2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+// Define the pins that we will use
+#define CAPTEUR 33
+#define LED 26
+#define DHTTYPE DHT11
+
+DHT dht(CAPTEUR, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-  printf("test");
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  dht.begin();
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  // Effectuer une mesure toutes les 5s
+  delay(5000);
+  
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  // Afficher l'humidité relative avec un peu de formatage
+  Serial.print("Humidité: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  
+  // Afficher la température avec un peu de formatage
+  Serial.print("Température: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  
+  Serial.println("-------------------");
 }

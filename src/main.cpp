a10c46289583bbae24cade2806323e3c5ce27c6d
AntoineBrosseau <antoine.brosseau@edu.ece.fr>
2026-01-14 15:24:51 +0100

    etape3

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,45 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-// Define the pins that we will use
 #define CAPTEUR 33
 #define LED 26
 #define DHTTYPE DHT11
+
+#define TIME_TO_SLEEP 5
+
+#define uS_TO_S_FACTOR 1000000ULL
 
 DHT dht(CAPTEUR, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  
   dht.begin();
-}
-
-void loop() {
-  // Effectuer une mesure toutes les 5s
-  delay(5000);
+  
+  delay(2000);
   
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
   
-  // Afficher l'humidité relative avec un peu de formatage
   Serial.print("Humidité: ");
   Serial.print(humidity);
   Serial.println(" %");
   
-  // Afficher la température avec un peu de formatage
   Serial.print("Température: ");
   Serial.print(temperature);
   Serial.println(" °C");
   
   Serial.println("-------------------");
+  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  
+  Serial.println("Passage en mode Deep Sleep pour 5 secondes...");
+  Serial.flush(); 
+  
+  esp_deep_sleep_start();
+}
+
+void loop() {
+
 }
